138802037f367281bbec5a500264d53a398b7288
gc-mcinnes <rory.mcinnes@nrcan-rncan.gc.ca>
2025-11-10 11:55:35 -0400

    Updated the make_random_ steps function

--- R/make_random_steps.R
+++ R/make_random_steps.R
@@ -2,11 +2,11 @@
 #' @export
 #' @author Julie W. Turner
 #' 
-make_random_steps <- function(DT) {
+make_random_steps <- function(DT, sl, ta) {
   if (is.null(DT)) return()
   if (nrow(DT) == 0) return()
 
-  random_steps(DT, n_control = 10) 
+  random_steps(DT, n_control = 10, sl_distr = sl, ta_distr = ta) 
   # %>%
   #   time_of_day(where = 'start', include.crepuscule = F)
 }

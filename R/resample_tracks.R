5bb554fcfc100ef2c2bdb2bb2bcad8ab25767bb0
gc-mcinnes <rory.mcinnes@nrcan-rncan.gc.ca>
2025-11-18 11:58:32 -0400

    Add a filter on the bursts
    removes bursts that dont have enough steps for the random steps function

--- R/resample_tracks.R
+++ R/resample_tracks.R
@@ -1,13 +1,19 @@
 #' @title check resample tracks
 #' @export
 #' @author Julie W. Turner
-#' 
+#'
 resample_tracks <- function(tracks, rate, tolerance, probsfilter) {
   t <- track_resample(tracks, rate = rate, tolerance = tolerance) %>%
     filter_min_n_burst()
-  
+
+  # remove bursts with less than 4 steps
+  t <- t %>%
+    dplyr::group_by(burst_) %>%
+    dplyr::filter(dplyr::n() >= 4) %>%
+    dplyr::ungroup()
+
   # Cancel if there are not at least 20 observed steps after resample
-  # this is semi-arbitrary, but this should be enough for robust estimates in the model 
+  # this is semi-arbitrary, but this should be enough for robust estimates in the model
   # (Street et al preprint 2021)
   if (nrow(t) < 20) return()
   # filter out steps that are outside the probsfilter%: there were unnaturally long steps
